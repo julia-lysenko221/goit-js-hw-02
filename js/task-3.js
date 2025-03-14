5669a1dde39c71796f910c3c7b058550b304098f
juliya.lysenko <juliya.deleh@gmail.com>
2025-03-14 14:18:21 +0200

    commit-1

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function checkForSpam(message) {
+  const x = message.toLowerCase().includes("sale");
+  const y = message.toLowerCase().includes("spam");
+  return x || y;
+}
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
